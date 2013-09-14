3125736f0b50a0b76e4be58881d969004cd6f884
Sebastian Gil Parga <s.gilparga@gmail.com>
2013-09-13 21:51:12 -0500

    Declaracion de los features y controller de materias

--- CupiCoordinador/app/controllers/materia_controller.rb
+++ CupiCoordinador/app/controllers/materia_controller.rb
@@ -1,2 +1,7 @@
 class MateriaController < ApplicationController
+  def MostrarMateria
+    if   params[:MateriaSeleccionada] != 'Seleccione una materia'
+      @materia = materias.Find( params[:MateriaSeleccionada])
+    end
+  end
 end
