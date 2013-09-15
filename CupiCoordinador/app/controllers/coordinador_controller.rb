37107af4b0d34ea1f7a88d5dc71e356786c08ea6
Ernesto Soto <e.soto162@uniandes.edu.co>
2013-09-15 12:26:42 -0500

    Interfaz para consolidado POR SEMESTRE, hay que arreglar la logica

--- CupiCoordinador/app/controllers/coordinador_controller.rb
+++ CupiCoordinador/app/controllers/coordinador_controller.rb
@@ -8,7 +8,13 @@ end
 
 def show_materia
 @semestres=Semestre.order("id_sem ASC").all
-@porMateria=EstudianteMateriaPeriodo.select("id_sem, idu, nombre, count(*) as num_estud").joins("join materia on estudiante_materia_periodos.id_materia=materia.id").joins("join semestres on estudiante_materia_periodos.id_semestre=semestres.id").where("estudiante_materia_periodos.id_materia='#{params[:materia]}'").group("id_estudiante")
+@porMateria=EstudianteMateriaPeriodo.select("id_sem, idu, nombre, count(*) as num_estud").joins("join materia on estudiante_materia_periodos.id_materia=materia.id").joins("join semestres on estudiante_materia_periodos.id_semestre=semestres.id").where("estudiante_materia_periodos.id_materia=?",params[:materia].to_f).group("id_estudiante")
+end
+
+def show_semestre
+@materias=Materia.order("nombre ASC").all
+@semester=params[:semestre_id].to_f
+@porSemestre=EstudianteMateriaPeriodo.select("id_sem, idu, nombre, count(*) as num_estud").joins("join materia on estudiante_materia_periodos.id_materia=materia.id").joins("join semestres on estudiante_materia_periodos.id_semestre=semestres.id").where("semestres.id=#{@semester}").group("id_estudiante")
 end
 
 end
