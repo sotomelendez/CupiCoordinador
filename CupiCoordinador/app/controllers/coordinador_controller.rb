eb28e37e693981c4d5d628f05d592ef4adfa6db8
Ernesto Soto <e.soto162@uniandes.edu.co>
2013-09-16 12:00:52 -0500

    Completado ver pensum y semestre tentativo, y en ver consolidado por semestre y por materia.

--- CupiCoordinador/app/controllers/coordinador_controller.rb
+++ CupiCoordinador/app/controllers/coordinador_controller.rb
@@ -3,18 +3,18 @@ class CoordinadorController < ApplicationController
 def consolidado
 @materias=Materia.order("nombre ASC").all
 @semestres=Semestre.order("id_sem ASC").all
-@resultado=EstudianteMateriaPeriodo.select("id_sem, idu, nombre, count(*) as num_estud").joins("join materia on estudiante_materia_periodos.id_materia=materia.id").joins("join semestres on estudiante_materia_periodos.id_semestre=semestres.id").group("id_estudiante")
+@resultado=EstudianteMateriaPeriodo.select("id_sem, idu, nombre, count(*) as num_estud").joins("join materia on estudiante_materia_periodos.id_materia=materia.id").joins("join semestres on estudiante_materia_periodos.id_semestre=semestres.id").group("id_semestre, id_materia").order("id_sem, nombre")
 end
 
 def show_materia
 @semestres=Semestre.order("id_sem ASC").all
-@porMateria=EstudianteMateriaPeriodo.select("id_sem, idu, nombre, count(*) as num_estud").joins("join materia on estudiante_materia_periodos.id_materia=materia.id").joins("join semestres on estudiante_materia_periodos.id_semestre=semestres.id").where("estudiante_materia_periodos.id_materia=?",params[:materia].to_f).group("id_estudiante")
+@porMateria=EstudianteMateriaPeriodo.select("id_sem, idu, nombre, count(*) as num_estud").joins("join materia on estudiante_materia_periodos.id_materia=materia.id").joins("join semestres on estudiante_materia_periodos.id_semestre=semestres.id").where("estudiante_materia_periodos.id_materia=?",params[:consolidado][:materia].to_f).group("id_semestre, id_materia").order("id_sem")
 end
 
 def show_semestre
 @materias=Materia.order("nombre ASC").all
-@semester=params[:semestre_id].to_f
-@porSemestre=EstudianteMateriaPeriodo.select("id_sem, idu, nombre, count(*) as num_estud").joins("join materia on estudiante_materia_periodos.id_materia=materia.id").joins("join semestres on estudiante_materia_periodos.id_semestre=semestres.id").where("semestres.id=#{@semester}").group("id_estudiante")
+@semester=params[:consolidado][:semestre_id].to_f
+@porSemestre=EstudianteMateriaPeriodo.select("id_sem, idu, nombre, count(*) as num_estud").joins("join materia on estudiante_materia_periodos.id_materia=materia.id").joins("join semestres on estudiante_materia_periodos.id_semestre=semestres.id").where("semestres.id=#{@semester}").group("id_semestre, id_materia").order("nombre")
 end
 
 end
--- CupiCoordinador/app/controllers/estudiante_controller.rb
+++ CupiCoordinador/app/controllers/estudiante_controller.rb
@@ -6,6 +6,10 @@ def semtent
 end
 
 def verpensum
+@estud=Estudiante.all.where(:stud_id => params[:pensum][:cod_stud]).limit(1)
+@estud.each do |est|
+@id_estud=est.id
+end
 @pensum=EstudianteMateriaPeriodo.select("id_sem, idu, nombre").joins("join materia on estudiante_materia_periodos.id_materia=materia.id").joins("join semestres on estudiante_materia_periodos.id_semestre=semestres.id").where("estudiante_materia_periodos.id_estudiante=#{@id_estud}").order("id_sem")
 end
 
