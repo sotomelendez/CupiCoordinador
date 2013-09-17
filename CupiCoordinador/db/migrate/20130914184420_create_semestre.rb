e31dcf7e542b9fa01d7ff732f004cd57dbfdece0
Ernesto Soto <e.soto162@uniandes.edu.co>
2013-09-16 22:32:45 -0500

    Merge

--- CupiCoordinador/db/migrate/20130914184420_create_semestre.rb
+++
@@ -1,8 +0,0 @@
-class CreateSemestre < ActiveRecord::Migration
-  def change
-    create_table :semestres do |t|
-       t.string :sem_id
-      
-    end
-  end
-end
--- CupiCoordinador/db/migrate/20130914184458_create_estudiante.rb
+++
@@ -1,8 +0,0 @@
-class CreateEstudiante < ActiveRecord::Migration
-  def change
-    create_table :estudiantes do |t|
-      t.string :stud_id
-      t.string :name
-    end
-  end
-end
