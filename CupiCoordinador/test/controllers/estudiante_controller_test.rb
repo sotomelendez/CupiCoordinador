c47c5e101d88818944ec88e39f5f1678e13bd27b
edgar sandoval <thrusthope@hotmail.com>
2013-09-18 00:34:51 -0500

    Completadas el resto de las pruebas de los controllers que quedaban mwith '#' will be ignored, and an empty message aborts the commit.

--- CupiCoordinador/test/controllers/estudiante_controller_test.rb
+++ CupiCoordinador/test/controllers/estudiante_controller_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class EstudianteControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get semestre tentativo" do
+    get :semtent
+    assert_response :success
+  end
+
+  test "should get pensum" do
+    get :verpensum
+    assert_response :success
+  end
+
+  test "should get by semestre" do
+    get :show_semestre
+    assert_response :success
+  end
+
+  test "should get by materia and semestre" do
+    get :show_materia_semestre
+    assert_response :success
+  end
+
 end
--- CupiCoordinador/test/controllers/semestre_controller_test.rb
+++ CupiCoordinador/test/controllers/semestre_controller_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class SemestreControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   test "should get new" do
+    get :new
+    assert_response :success
+  end
+
+  test "should get show" do
+    get :show
+    assert_response :success
+  end
+
+
 end
