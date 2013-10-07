d265b1764896e974bddb5bb09b47f72be74d8aae
Ernesto Soto <e.soto162@uniandes.edu.co>
2013-10-07 17:39:30 -0500

    Avance en la funcion de asignacion

--- CupiCoordinador/app/controllers/asignacion_controller.rb
+++ CupiCoordinador/app/controllers/asignacion_controller.rb
@@ -7,19 +7,19 @@ class AsignacionController < ApplicationController
   def resultado
 	if Asignacion.count("id_materia=#{params[:asign][:materia]} AND id_semestre=#{params[:asign][:semestre]}")==0
 	{
-		
+		render :action => 'ReservarPrograma'
+		render :action => 'ReservarOtroPrograma'
+		render :action => 'ReserverPregrado'
 	}
-	else
-	{
-		
-	}
-	render :action => 'ReservarPrograma'
-	render :action => 'ReservarOtroPrograma'
-	render :action => 'ReserverPregrado'
+	
   end
   
   def ReservarPrograma
-  
+	@estuds = Estudiante.select("id, name").joins("join estudiante_materia_periodos on estudiantes.id=estudiante_materia_periodos.id_estudiante").where("estudiante_materia_periodos.id_materia=#{params[:asign][:materia]} AND estudiante_materia_periodos.id_semestre=#{params[:asign][:semestre]}").order("random()").limit(10)
+	@estuds.each do |est|
+		@asignado = Asignacion.new(params[:asign][:materia],est.id,params[:asign][:semestre])
+		@asignado.save
+	end
   end
   
   def ReservarPregrado
