994e3ff6864ad4c92591aa1f59dc8f456603179f
Ernesto Soto <e.soto162@uniandes.edu.co>
2013-10-08 19:40:03 -0500

    Ya casiii!!!

--- CupiCoordinador/app/controllers/asignacion_controller.rb
+++ CupiCoordinador/app/controllers/asignacion_controller.rb
@@ -15,8 +15,9 @@ class AsignacionController < ApplicationController
   end
   
   def ReservarPrograma
-	@estuds = EstudianteMateriaPeriodo.select("estudiantes.id as id, name").joins("join estudiantes on estudiante_materia_periodos.id_estudiante=estudiantes.id").joins("join estudia on estudiante_materia_periodos.id_estudiante=estudia.id_estudiante").joins("join materia_maestria on estudiante_materia_periodos.id_materia=materia_maestria.id_materia").where("estudiante_materia_periodos.id_materia=#{params[:materia]} AND estudiante_materia_periodos.id_semestre=#{params[:semestre]} AND estudia.id_maestria=materia_maestria.id_maestria").order("random()").limit(10)
-	@estuds.each do |est|
+	@cupos=Cupos.select("num_cupos").where("id_materia=#{params[:materia]}")
+	@estuds = EstudianteMateriaPeriodo.select("estudiantes.id as id, name").joins("join estudiantes on estudiante_materia_periodos.id_estudiante=estudiantes.id").joins("join estudia on estudiante_materia_periodos.id_estudiante=estudia.id_estudiante").joins("join materia_maestria on estudiante_materia_periodos.id_materia=materia_maestria.id_materia").where("estudiante_materia_periodos.id_materia=#{params[:materia]} AND estudiante_materia_periodos.id_semestre=#{params[:semestre]} AND estudia.id_maestria=materia_maestria.id_maestria").order("random()")
+	@estuds.first((@cupos.first.num_cupos*0.5).to_i).each do |est|
 		@asignado = Asignacion.new(:id_materia => params[:materia].to_i, :id_estudiante => est.id, :id_semestre => params[:semestre].to_i)
 		@asignado.save
 	end
@@ -24,17 +25,19 @@ class AsignacionController < ApplicationController
   end
   
   def ReservarPregrado
-	@estuds = EstudianteMateriaPeriodo.select("estudiantes.id as id, name").joins("join estudiantes on estudiante_materia_periodos.id_estudiante=estudiantes.id").joins("join estudia on estudiante_materia_periodos.id_estudiante=estudia.id_estudiante").joins("join materia_maestria on estudiante_materia_periodos.id_materia=materia_maestria.id_materia").where("estudiante_materia_periodos.id_materia=#{params[:materia]} AND estudiante_materia_periodos.id_semestre=#{params[:semestre]} AND estudia.id_maestria=1").order("random()").limit(4)
-	@estuds.each do |est|
+	@cupos=Cupos.select("num_cupos").where("id_materia=#{params[:materia]}")
+	@estuds = EstudianteMateriaPeriodo.select("estudiantes.id as id, name").joins("join estudiantes on estudiante_materia_periodos.id_estudiante=estudiantes.id").joins("join estudia on estudiante_materia_periodos.id_estudiante=estudia.id_estudiante").joins("join materia_maestria on estudiante_materia_periodos.id_materia=materia_maestria.id_materia").where("estudiante_materia_periodos.id_materia=#{params[:materia]} AND estudiante_materia_periodos.id_semestre=#{params[:semestre]} AND estudia.id_maestria=1").order("random()")
+	@estuds.first((@cupos.first.num_cupos*0.2).to_i).each do |est|
 		@asignado = Asignacion.new(:id_materia => params[:materia].to_i, :id_estudiante => est.id, :id_semestre => params[:semestre].to_i)
 		@asignado.save
 	end
-	redirect_to :action => 'show',  :params => { :semestre => params[:semestre], :materia => params[:materia] }
+	redirect_to :action => 'show', :params => { :semestre => params[:semestre], :materia => params[:materia] }
   end
   
   def ReservarOtroPrograma
-	@estuds = EstudianteMateriaPeriodo.select("estudiantes.id as id, name").joins("join estudiantes on estudiante_materia_periodos.id_estudiante=estudiantes.id").joins("join estudia on estudiante_materia_periodos.id_estudiante=estudia.id_estudiante").joins("join materia_maestria on estudiante_materia_periodos.id_materia=materia_maestria.id_materia").where("estudiante_materia_periodos.id_materia=#{params[:materia]} AND estudiante_materia_periodos.id_semestre=#{params[:semestre]} AND estudia.id_maestria<>materia_maestria.id_maestria AND estudia.id_maestria<>1").order("random()").limit(6)
-	@estuds.each do |est|
+	@cupos=Cupos.select("num_cupos").where("id_materia=#{params[:materia]}")
+	@estuds = EstudianteMateriaPeriodo.select("estudiantes.id as id, name").joins("join estudiantes on estudiante_materia_periodos.id_estudiante=estudiantes.id").joins("join estudia on estudiante_materia_periodos.id_estudiante=estudia.id_estudiante").joins("join materia_maestria on estudiante_materia_periodos.id_materia=materia_maestria.id_materia").where("estudiante_materia_periodos.id_materia=#{params[:materia]} AND estudiante_materia_periodos.id_semestre=#{params[:semestre]} AND estudia.id_maestria<>materia_maestria.id_maestria AND estudia.id_maestria<>1").order("random()")
+	@estuds.first((@cupos.first.num_cupos*0.3).to_i).each do |est|
 		@asignado = Asignacion.new(:id_materia => params[:materia].to_i, :id_estudiante => est.id, :id_semestre => params[:semestre].to_i)
 		@asignado.save
 	end
@@ -42,6 +45,7 @@ class AsignacionController < ApplicationController
   end
   
   def show
+	@cupos=Cupos.select("num_cupos").where("id_materia=#{params[:materia]}")
 	@materia=Materia.find(params[:materia].to_i)
 	@semestre=Semestre.find(params[:semestre].to_i)
 	@resultado = Asignacion.select("estudiantes.name as name, estudiantes.stud_id as stud_id, maestria.nombre as maest").joins("join estudiantes on asignacions.id_estudiante=estudiantes.id").joins("join estudia on asignacions.id_estudiante=estudia.id_estudiante").joins("join maestria on estudia.id_maestria=maestria.id").where("asignacions.id_materia=#{params[:materia].to_i} AND asignacions.id_semestre=#{params[:semestre].to_i}").order("maest")
