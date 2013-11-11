54d3dea59c2024cd7fd7f63e914f34f0481fef98
esandoval2 <thrusthope@hotmail.com>
2013-11-11 14:47:50 -0500

    Interfaz modernizada y modificada! solo quedaron 2 pestañas...navegar entre ellas y veran las nuevas funcionalidades.

--- CupiCoordinador/app/controllers/coordinador_controller.rb
+++ CupiCoordinador/app/controllers/coordinador_controller.rb
@@ -9,34 +9,35 @@ begin
 
 puts "CONNNNN========================= #{params[:fil1]}======#{params[:fil2]}"
 
-if  params[:consolidado][:filsemestre]== 0 and params[:consolidado][:filmateria] == 0
+if  !params[:fil1] and !params[:fil2]
 
 @busqueda=EstudianteMateriaPeriodo.select("id_sem, id_materia,id_semestre,idu, nombre, count(*) as num_estud").joins("join materia on estudiante_materia_periodos.id_materia=materia.id").joins("join semestres on estudiante_materia_periodos.id_semestre=semestres.id").group("id_semestre, id_materia").order("id_sem, nombre")
 @cuentamaestrias=EstudianteMateriaPeriodo.select("id_maestria,codigo,nombre,count(*) as ct").joins("join estudiantes on estudiante_materia_periodos.id_estudiante=estudiantes.id").joins("join estudia on estudiantes.id=estudia.id_estudiante").joins("join maestria on estudia.id_maestria=maestria.id").group("id_maestria")
 @cuentaantiguos=Estudiante.select("id_estudiante,apellidos").joins("join estudiante_materia_periodos on estudiante_materia_periodos.id_estudiante=estudiantes.id")
-@semantigu=@semestres.last
+@semantigu=@semestres.last.id_sem
 
-elsif  params[:consolidado][:filsemestre]== 1 and params[:consolidado][:filmateria] == 0
+elsif  !params[:fil1] and params[:fil2]
+@semester=params[:consolidado][:semestre_id].to_i
 
-@busqueda=EstudianteMateriaPeriodo.select("id_sem, id_materia,id_semestre,idu, nombre, count(*) as num_estud").joins("join materia on estudiante_materia_periodos.id_materia=materia.id").joins("join semestres on estudiante_materia_periodos.id_semestre=semestres.id").group("id_semestre, id_materia").order("id_sem, nombre")
-@cuentamaestrias=EstudianteMateriaPeriodo.select("id_maestria,codigo,nombre,count(*) as ct").joins("join estudiantes on estudiante_materia_periodos.id_estudiante=estudiantes.id").joins("join estudia on estudiantes.id=estudia.id_estudiante").joins("join maestria on estudia.id_maestria=maestria.id").group("id_maestria")
-@cuentaantiguos=Estudiante.select("id_estudiante,apellidos").joins("join estudiante_materia_periodos on estudiante_materia_periodos.id_estudiante=estudiantes.id")
-@semantigu=@semestres.last
+@busqueda=EstudianteMateriaPeriodo.select("id_sem, id_materia,id_semestre,idu, nombre, count(*) as num_estud").joins("join materia on estudiante_materia_periodos.id_materia=materia.id").joins("join semestres on estudiante_materia_periodos.id_semestre=semestres.id").group("id_semestre, id_materia").where("semestres.id=#{@semester}").order("nombre")
+@cuentamaestrias=EstudianteMateriaPeriodo.select("id_maestria,codigo,nombre,count(*) as ct").joins("join estudiantes on estudiante_materia_periodos.id_estudiante=estudiantes.id").joins("join estudia on estudiantes.id=estudia.id_estudiante").joins("join maestria on estudia.id_maestria=maestria.id").where("estudiante_materia_periodos.id_semestre=#{@semester}").group("id_maestria")
+@cuentaantiguos=Estudiante.select("id_estudiante,apellidos").joins("join estudiante_materia_periodos on estudiante_materia_periodos.id_estudiante=estudiantes.id").where("estudiante_materia_periodos.id_semestre=#{@semester}")
+@semantigu=Semestre.find(params[:consolidado][:semestre_id]).id_sem
 
 elsif   params[:fil1] and !params[:fil2]
 
 @busqueda=EstudianteMateriaPeriodo.select("id_sem, id_materia,id_semestre,idu, nombre, count(*) as num_estud").joins("join materia on estudiante_materia_periodos.id_materia=materia.id").joins("join semestres on estudiante_materia_periodos.id_semestre=semestres.id").where("estudiante_materia_periodos.id_materia=?",params[:consolidado][:materia].to_i).group("id_semestre, id_materia").order("id_sem")
 @cuentamaestrias=EstudianteMateriaPeriodo.select("id_maestria,codigo,nombre,count(*) as ct").joins("join estudiantes on estudiante_materia_periodos.id_estudiante=estudiantes.id").joins("join estudia on estudiantes.id=estudia.id_estudiante").joins("join maestria on estudia.id_maestria=maestria.id").where("estudiante_materia_periodos.id_materia=?",params[:consolidado][:materia].to_i).group("id_maestria")
 @cuentaantiguos=Estudiante.select("id_estudiante,apellidos").joins("join estudiante_materia_periodos on estudiante_materia_periodos.id_estudiante=estudiantes.id").where("estudiante_materia_periodos.id_materia=?",params[:consolidado][:materia].to_i)
-@semantigu=@semestres.last
+@semantigu=@semestres.last.id_sem
 
 
-elsif  params[:consolidado][:filsemestre]== 1 and params[:consolidado][:filmateria] == 1
-
-@busqueda=EstudianteMateriaPeriodo.select("id_sem, id_materia,id_semestre,idu, nombre, count(*) as num_estud").joins("join materia on estudiante_materia_periodos.id_materia=materia.id").joins("join semestres on estudiante_materia_periodos.id_semestre=semestres.id").group("id_semestre, id_materia").order("id_sem, nombre")
-@cuentamaestrias=EstudianteMateriaPeriodo.select("id_maestria,codigo,nombre,count(*) as ct").joins("join estudiantes on estudiante_materia_periodos.id_estudiante=estudiantes.id").joins("join estudia on estudiantes.id=estudia.id_estudiante").joins("join maestria on estudia.id_maestria=maestria.id").group("id_maestria")
-@cuentaantiguos=Estudiante.select("id_estudiante,apellidos").joins("join estudiante_materia_periodos on estudiante_materia_periodos.id_estudiante=estudiantes.id")
-@semantigu=@semestres.last
+elsif  params[:fil1] and params[:fil2]
+@semester=params[:consolidado][:semestre_id].to_i
+@busqueda=EstudianteMateriaPeriodo.select("id_sem, id_materia,id_semestre,idu, nombre, count(*) as num_estud").joins("join materia on estudiante_materia_periodos.id_materia=materia.id").joins("join semestres on estudiante_materia_periodos.id_semestre=semestres.id").where("estudiante_materia_periodos.id_materia=#{params[:consolidado][:materia].to_i} AND estudiante_materia_periodos.id_semestre=#{params[:consolidado][:semestre_id].to_i}").group("id_semestre, id_materia").order("id_sem")
+@cuentamaestrias=EstudianteMateriaPeriodo.select("id_maestria,codigo,nombre,count(*) as ct").joins("join estudiantes on estudiante_materia_periodos.id_estudiante=estudiantes.id").joins("join estudia on estudiantes.id=estudia.id_estudiante").joins("join maestria on estudia.id_maestria=maestria.id").where("estudiante_materia_periodos.id_materia=#{params[:consolidado][:materia].to_i} AND estudiante_materia_periodos.id_semestre=#{params[:consolidado][:semestre_id].to_i}").group("id_maestria")
+@cuentaantiguos=Estudiante.select("id_estudiante,apellidos").joins("join estudiante_materia_periodos on estudiante_materia_periodos.id_estudiante=estudiantes.id").where("estudiante_materia_periodos.id_materia=#{params[:consolidado][:materia].to_i} AND estudiante_materia_periodos.id_semestre=#{params[:consolidado][:semestre_id].to_i}")
+@semantigu=Semestre.find(params[:consolidado][:semestre_id]).id_sem
 
 end
 rescue
@@ -45,7 +46,7 @@ puts "Error========================= #{$!}"
 @busqueda=EstudianteMateriaPeriodo.select("id_sem, id_materia,id_semestre,idu, nombre, count(*) as num_estud").joins("join materia on estudiante_materia_periodos.id_materia=materia.id").joins("join semestres on estudiante_materia_periodos.id_semestre=semestres.id").group("id_semestre, id_materia").order("id_sem, nombre")
 @cuentamaestrias=EstudianteMateriaPeriodo.select("id_maestria,codigo,nombre,count(*) as ct").joins("join estudiantes on estudiante_materia_periodos.id_estudiante=estudiantes.id").joins("join estudia on estudiantes.id=estudia.id_estudiante").joins("join maestria on estudia.id_maestria=maestria.id").group("id_maestria")
 @cuentaantiguos=Estudiante.select("id_estudiante,apellidos").joins("join estudiante_materia_periodos on estudiante_materia_periodos.id_estudiante=estudiantes.id")
-@semantigu=@semestres.last
+@semantigu=@semestres.last.id_sem
 end
 end
 
